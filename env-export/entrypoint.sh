b7abf48d40879d046f722d926ef686b998374714
Floris van der Grinten <floris@grinten.com>
2020-03-02 09:11:01 +0100

    First mask, then set variable

--- env-export/entrypoint.sh
+++ env-export/entrypoint.sh
@@ -5,6 +5,6 @@ env_var_names=$(secrethub env ls)
 
 for var_name in $env_var_names; do
 	secret_value=$(secrethub env read $var_name)
-	echo "::set-env name=$var_name::$secret_value"
 	echo "::add-mask::$secret_value"
+	echo "::set-env name=$var_name::$secret_value"
 done
