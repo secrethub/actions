537b38da0a79e603c2e5d7ffd240a7fc25730ba8
Floris van der Grinten <floris@grinten.com>
2020-03-03 11:24:04 +0100

    Log secret variable names

--- env-export/entrypoint.sh
+++ env-export/entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 env_var_names=$(secrethub env ls)
+echo "The following environment variables will be populated with secrets from SecretHub:"
+echo "$env_var_names"
 
 for var_name in $env_var_names; do
 	secret_value=$(secrethub env read $var_name)
