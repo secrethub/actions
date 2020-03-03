d1ad1f8fdbfe0562a23487d15bb16b10dc59ea05
Floris van der Grinten <floris@grinten.com>
2020-03-03 14:38:39 +0100

    Support newlines and percent signs in secrets

--- env-export/entrypoint.sh
+++ env-export/entrypoint.sh
@@ -7,6 +7,16 @@ echo "$env_var_names"
 
 for var_name in $env_var_names; do
 	secret_value=$(secrethub env read $var_name)
-	echo "::add-mask::$secret_value"
-	echo "::set-env name=$var_name::$secret_value"
+
+	# Escape percent signs and add a mask per line (see https://github.com/actions/runner/issues/161)
+	escaped_mask_value=$(echo "$secret_value" | sed -e 's/%/%25/g')
+	IFS=$'\n'
+	for line in $escaped_mask_value; do
+		echo "::add-mask::$line"
+	done
+	unset IFS
+
+	# Escape percent signs and newlines when setting the environment variable
+	escaped_env_var_value=$(echo -n "$secret_value" | sed -z -e 's/%/%25/g' -e 's/\n/%0A/g')
+	echo "::set-env name=$var_name::$escaped_env_var_value"
 done
